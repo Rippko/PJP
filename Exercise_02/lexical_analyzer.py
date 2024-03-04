c8ae2d8496b86d50cd4e85a8f83b2ccce30a0bb4
Adam Fiury <adam.mullermilch@gmail.com>
2024-03-04 09:19:19 +0100

    Added token class to represent each token

--- Exercise_02/lexical_analyzer.py
+++ Exercise_02/lexical_analyzer.py
@@ -1,4 +1,6 @@
+from token import Token
 import re
+
 
 def tokenize(input_text):
     identifier_regex = re.compile(r'^[a-zA-Z][a-zA-Z0-9]*$')
@@ -19,23 +21,23 @@ def tokenize(input_text):
             if symbol.isspace():
                 continue
             elif identifier_regex.match(symbol) and not keyword_regex.match(symbol):
-                tokens.append(f'ID: {symbol}')
+                tokens.append(Token('ID', symbol))
             elif number_regex.match(symbol):
-                tokens.append(f'NUM: {symbol}')
+                tokens.append(Token('NUM', symbol))
             elif operator_regex.match(symbol):
-                tokens.append(f'OP: {symbol}')
+                tokens.append(Token('OP', symbol))
             elif delimiter_regex.match(symbol):
                 if symbol == ';':
-                    tokens.append('SEMICOLON')
+                    tokens.append(Token('SEMICOLON', None))
                 elif symbol == '(':
-                    tokens.append('LPAR')
+                    tokens.append(Token('LPAR', None))
                 else:
-                    tokens.append('RPAR')
+                    tokens.append(Token('RPAR', None))
             elif keyword_regex.match(symbol):
                 if symbol == 'div':
-                    tokens.append('DIV')
+                    tokens.append(Token('DIV', None))
                 else:
-                    tokens.append('MOD')
+                    tokens.append(Token('MOD', None))
             else:
                 print(f'Unknown token: {symbol}')
 
