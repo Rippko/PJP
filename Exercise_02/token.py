91097ed57c3ca78985deb004ba6d5cebf5a7afa9
Adam Fiury <adam.mullermilch@gmail.com>
2024-03-04 09:19:45 +0100

    Token class

---
+++ Exercise_02/token.py
@@ -0,0 +1,10 @@
+class Token():
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        if self.value:
+            return f"{self.type}: {self.value}"
+        else:
+            return f"{self.type}"
